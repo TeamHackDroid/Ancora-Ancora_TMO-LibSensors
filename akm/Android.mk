b267530c96c2103edb2f8d33332ad769eae2ff57
Doadin <tbkizle@gmail.com>
2014-05-16 19:23:28 -0400

    Should be set to compile

--- akm/Android.mk
+++ akm/Android.mk
@@ -1,9 +1,46 @@
-# TODO: Change device number!
+ifneq ($(TARGET_PROVIDES_LIBSENSORS),true)
 
-#export AKMD_DEVICE_TYPE=8975
-#export AKMD_DEVICE_TYPE=8963
-#export AKMD_DEVICE_TYPE=8975
-#export AKMD_DEVICE_TYPE=9911
-#export AKMD_SENSOR_ACC=adxl346
-hardware_modules := akmdfs libsensors
-include $(call all-named-subdir-makefiles,$(hardware_modules))
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+
+LOCAL_CFLAGS := -DLOG_TAG=\"Sensors\" \
+                -Wall -Wextra
+
+LOCAL_SRC_FILES := \
+        InputEventReader.cpp \
+        AkmSensor.cpp \
+        akmdfs/AKFS_APIs_8975/AKFS_AK8975.c \
+        akmdfs/AKFS_APIs_8975/AKFS_AOC.c \
+        akmdfs/AKFS_APIs_8975/AKFS_Device.c \
+        akmdfs/AKFS_APIs_8975/AKFS_Direction.c \
+        akmdfs/AKFS_APIs_8975/AKFS_VNorm.c \
+        akmdfs/AKFS_FileIO.c \
+        sensors.cpp         \
+        SensorBase.cpp      \
+        LightSensor.cpp     \
+        ProximitySensor.cpp \
+        Bma222Sensor.cpp    \
+        OrientationSensor.cpp   \
+        InputEventReader.cpp
+            
+LOCAL_C_INCLUDES := \
+        $(LOCAL_PATH)/akmdfs \
+        $(LOCAL_PATH)/akmdfs/AKFS_APIs_8975 \
+        $(LOCAL_PATH)
+
+LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
+LOCAL_PRELINK_MODULE := false
+
+
+#
+# Specify device type
+#
+
+LOCAL_CFLAGS  += -DHAL_FOR_AK8975
+
+LOCAL_MODULE := sensors.$(TARGET_BOOTLOADER_BOARD_NAME)
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
